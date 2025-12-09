b0da660d561820390bfe86db795e36d15f870492
ankitsanjai-max <ankit.sanjai@gmail.com>
2025-12-09 05:30:18 +0000

    day2

---
+++ Basic_JS/Day2.js
@@ -0,0 +1,45 @@
+// let a=23
+// const b="abc"
+// var c=78
+// d=88
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// a=67
+// b="xyz"
+// c=99
+// d=100
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// let a=11
+// const b="def"
+// var c=22
+// d=33
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// let a="abc"
+// let b=""
+// let c=null
+// let d
+// console.log(a,b,c,d)
+
+// console.log(typeof(d))
+
+let score="33abcd"
+console.log(typeof(score))
+console.log(typeof score)
+
+let a=Number(score)
+console.log(typeof(a))
+console.log(a)
