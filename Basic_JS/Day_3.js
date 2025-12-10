ff6fe983c302442402b636859f03b73af10c59e0
ankitsanjai-max <ankit.sanjai@gmail.com>
2025-12-10 05:33:38 +0000

    day 3

---
+++ Basic_JS/Day_3.js
@@ -0,0 +1,35 @@
+// function fun(n){
+//     return n*2
+// }
+// let ch=fun(72)
+// console.log(ch)
+
+// let arr=[1,2,3,4,5,"ankit","sharan",ch]
+// console.log(arr)
+
+// let fruits=["apple","banana","peach","orange","grapes"]
+// for(let i=0;i<fruits.length;i++)
+//     console.log(fruits[i])
+// for(let a of fruits)
+//     console.log(a)
+
+// fruits.push("Papaya")
+// fruits.pop()
+// fruits.unshift("Kiwi")
+// fruits.shift()
+// console.log(fruits.includes("mango"))
+// console.log(fruits.indexOf("banana"))
+
+
+// Higher-Oreder Array Functions
+
+// let numbers=[2,4,6,8]
+// let doubled=numbers.map(n=>n*2)
+// console.log(doubled)
+// let num1=[10,25,30,5,60]
+// let result=num1.filter(n=>n>20)
+// console.log(result)
+// let num2=[1,2,3,4,5]
+// let total=num2.reduce((acc,val)=>acc+val,0)
+// console.log(total)
+
